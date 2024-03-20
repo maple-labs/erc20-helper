d448b27ad9f51a9b677bbc2842afa905bd7458a5
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-03-20 13:31:37 -0600

    feat: add permit

--- src/MockERC20.sol
+++ src/MockERC20.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.0;
 
+import "forge-std/Test.sol";
+
 contract MockERC20 {
 
     string public name;
@@ -13,6 +15,16 @@ contract MockERC20 {
     mapping(address => uint256) public balanceOf;
 
     mapping(address => mapping(address => uint256)) public allowance;
+
+    /**********************************************************************************************/
+    /*** External Functions                                                                     ***/
+    /**********************************************************************************************/
+
+    // PERMIT_TYPEHASH = keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+    bytes32 public constant PERMIT_TYPEHASH
+        = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
+
+    mapping(address => uint256) public nonces;
 
     constructor(string memory name_, string memory symbol_, uint8 decimals_) {
         name     = name_;
@@ -42,6 +54,45 @@ contract MockERC20 {
         _approve(msg.sender, spender_, allowance[msg.sender][spender_] + addedAmount_);
         return true;
     }
+
+    function permit(address owner_,
+        address spender_,
+        uint256 amount_,
+        uint256 deadline_,
+        uint8 v_,
+        bytes32 r_,
+        bytes32 s_
+    )
+        public
+    {
+        require(deadline_ >= block.timestamp, "ERC20:P:EXPIRED");
+
+        // Appendix F in the Ethereum Yellow paper (https://ethereum.github.io/yellowpaper/paper.pdf), defines
+        // the valid range for s in (301): 0 < s < secp256k1n ÷ 2 + 1, and for v in (302): v ∈ {27, 28}.
+        require(
+            uint256(s_) <= uint256(0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0) &&
+            (v_ == 27 || v_ == 28),
+            "ERC20:P:MALLEABLE"
+        );
+
+        // Nonce realistically cannot overflow.
+        unchecked {
+            bytes32 digest = keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    DOMAIN_SEPARATOR(),
+                    keccak256(abi.encode(PERMIT_TYPEHASH, owner_, spender_, amount_, nonces[owner_]++, deadline_))
+                )
+            );
+
+            address recoveredAddress = ecrecover(digest, v_, r_, s_);
+
+            require(recoveredAddress == owner_ && owner_ != address(0), "ERC20:P:INVALID_SIGNATURE");
+        }
+
+        _approve(owner_, spender_, amount_);
+    }
+
     function transfer(address recipient_, uint256 amount_) public virtual returns (bool success_) {
         _transfer(msg.sender, recipient_, amount_);
         return true;
@@ -53,6 +104,22 @@ contract MockERC20 {
         _decreaseAllowance(owner_, msg.sender, amount_);
         _transfer(owner_, recipient_, amount_);
         return true;
+    }
+
+    /**********************************************************************************************/
+    /*** View Functions                                                                         ***/
+    /**********************************************************************************************/
+
+    function DOMAIN_SEPARATOR() public view returns (bytes32 domainSeparator_) {
+        return keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256(bytes(name)),
+                keccak256(bytes("1")),
+                block.chainid,
+                address(this)
+            )
+        );
     }
 
     /**********************************************************************************************/
